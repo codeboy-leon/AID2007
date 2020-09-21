deacca15f8dab9210a7e825aeba089f4b7e9c697
leon <ljc951010@163.com>
2020-09-21 09:58:36 +0800

    sss

--- test/student_controller.py
+++ test/student_controller.py
@@ -2,18 +2,12 @@ class Controller():
 
     def show_students(self,data):
         print('''
--------------
-|所有学生信息：|
--------------
+        -------------
+        |所有学生信息：|
+        -------------
         ''')
         self.print_info(data)
 
-    # def select_student(self,cur):
-    #     self.choose = input('请输入你的选择:')
-    #     if self.choose == '1':
-    #         id = input('请输入学生ID:')
-    #         cur.execute('select * from cls where id=%s',[id])
-    #         return cur.fetchall()
     def print_info(self,data):
         for item in data:
             title=['学生ID:','学生姓名:','年龄:','性别:','分数:']
--- test/temp.py
+++ test/temp.py
@@ -1,27 +1,17 @@
-from socket import *
-from select import poll
+import re
 
-sk = socket()
-sk.bind(('0.0.0.0',8848))
-sk.listen(5)
+ip = 'ip:192.168.122.20  ip:154.554.744.44 ip:192.200.355.155 ip:192.168.22.1'
 
-p = poll()
-map = {}
-map[sk.fileno()] = sk
-p.resgiter(sk,POLLIN)
-sk.setblocking(False)
 
-# while True:
-    events = p.poll()
-    for fd,event in events:
-        if fd == sk.fileno():
-            connfd,addr = map[fd].accept()
-            p.resgiter(connfd)
-            map[connfd.fileno()] = connfd
-        elif event == POLLIN:
-            data = map[fd].recv(1024).decode()
-            if not data:
-                map[fd].close()
-                p.unresgiter(map[fd])
-                map[fd].remove()
-            print(data)
+try:
+    result = re.findall(r'(([0-2]\d{1,2}\.){3}[0-2]\d{1,2})',ip)
+    print(result)
+except:
+    print('没有匹配到IP地址')
+# s = 'GET 12345'
+# 
+#
+# ret = re.match('\w+',s).group()
+# print(ret)
+# ret = re.match('(\w+) (/d)',s).group()
+# print(ret)
